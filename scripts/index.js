19572f834daf1138727ab90bd2213a6a7c00b685
Maxim P <raiyin@ya.ru>
2023-01-18 00:27:14 +0300

    All done as needed.

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ import { createRandomColor } from './utils/utils.js';
 
 let lastDraggableNodeId = 0;
 let creator = document.getElementById('creator');
-let portable = null;
 let lastDroppable = null;
+let portable = null;
 
 function genDraggableNodeId() {
     lastDraggableNodeId++;
@@ -11,76 +11,75 @@ function genDraggableNodeId() {
 }
 
 function createDraggableNode() {
-    portable = document.createElement('div');
-    portable.id = genDraggableNodeId();
-    portable.classList += "portable";
-    portable.setAttribute("draggable", "true");
-    portable.style.backgroundColor = createRandomColor();
-    portable.addEventListener('pointerdown', pointerdown);
-    portable.ondragstart = function () {
-        return false;
-    };
-    return portable;
+    let node = document.createElement('div');
+    node.id = genDraggableNodeId();
+    node.classList += "portable";
+    node.setAttribute("draggable", "true");
+    node.style.backgroundColor = createRandomColor();
+    node.addEventListener('pointerdown', pointerdown);
+    node.ondragstart = () => false;
+    return node;
+}
+
+function setMovingStyle(element) {
+    element.style.boxShadow = '5px 5px 5px grey';
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+}
+
+function removeMovingStyle(element, ordered) {
+    element.setAttribute("draggable", "false");
+    element.style.boxShadow = '';
+    element.style.cursor = 'default';
+    element.onpointerup = null;
+    element.style.position = ordered ? 'static' : 'absolute';
+}
+
+function enterDroppable(element) {
+    element.style.cursor = 'copy';
+}
+
+function leaveDroppable(element) {
+    element.style.cursor = 'move';
+}
+
+function setChaosCoords(element, event) {
+    element.style.left = parseInt(element.style.left, 10) - event.target.offsetParent.offsetLeft + 'px';
+    element.style.top = parseInt(element.style.top, 10) - event.target.offsetParent.offsetTop + 'px';
+}
+
+function determineCurrentDroppable(ev) {
+    let elementBelow;
+    try {
+        ev.target.hidden = true;
+        elementBelow = document.elementFromPoint(ev.clientX, ev.clientY);
+        ev.target.hidden = false;
+    }
+    catch (e) { return; }
+
+    if (!elementBelow) return;
+    let droppableBelow = elementBelow.closest('.droppable');
+
+    if (droppableBelow != lastDroppable) {
+        if (lastDroppable) leaveDroppable(ev.target);
+        lastDroppable = droppableBelow;
+        if (lastDroppable) enterDroppable(ev.target);
+    }
 }
 
 function pointerdown(event) {
     event.preventDefault();
-    let shiftX = event.clientX - portable.getBoundingClientRect().left;
-    let shiftY = event.clientY - portable.getBoundingClientRect().top;
+    let shiftX = event.clientX - this.getBoundingClientRect().left;
+    let shiftY = event.clientY - this.getBoundingClientRect().top;
 
-    setMovingStyle(portable);
-    document.body.append(portable);
-    moveAt(portable, event.pageX, event.pageY);
+    setMovingStyle(this);
+    document.body.append(this);
+    moveAt(this, event.pageX, event.pageY);
     determineCurrentDroppable(event);
-
-    function setMovingStyle(element) {
-        element.style.boxShadow = '5px 5px 5px grey';
-        element.style.position = 'absolute';
-        element.style.zIndex = 1000;
-    }
-
-    function removeMovingStyle(element, ordered) {
-        element.setAttribute("draggable", "false");
-        element.style.boxShadow = '';
-        element.style.cursor = 'default';
-        element.onpointerup = null;
-        element.style.position = ordered ? 'static' : 'absolute';
-    }
 
     function moveAt(element, pageX, pageY) {
         element.style.left = pageX - shiftX + 'px';
         element.style.top = pageY - shiftY + 'px';
-    }
-
-    function enterDroppable(element) {
-        element.style.cursor = 'copy';
-    }
-
-    function leaveDroppable(element) {
-        element.style.cursor = 'move';
-    }
-
-    function setChaosCoords(element, event) {
-        // element.style.left = event.clientX - shiftX - event.target.offsetParent.offsetLeft + 'px';
-        // element.style.top = event.clientY - shiftY - event.target.offsetParent.offsetTop + 'px';
-
-        element.style.left = parseInt(element.style.left, 10) - event.target.offsetParent.offsetLeft + 'px';
-        element.style.top = parseInt(element.style.top, 10) - event.target.offsetParent.offsetTop + 'px';
-    }
-
-    function determineCurrentDroppable(ev) {
-        portable.hidden = true;
-        let elementBelow = document.elementFromPoint(ev.clientX, ev.clientY);
-        portable.hidden = false;
-
-        if (!elementBelow) return;
-        let droppableBelow = elementBelow.closest('.droppable');
-
-        if (droppableBelow != lastDroppable) {
-            if (lastDroppable) leaveDroppable(portable);
-            lastDroppable = droppableBelow;
-            if (lastDroppable) enterDroppable(portable);
-        }
     }
 
     function pointermove(event) {
@@ -89,8 +88,9 @@ function pointerdown(event) {
         determineCurrentDroppable(event);
     }
 
-    portable.onpointerup = function (event) {
+    function pointerup(event) {
         document.removeEventListener('pointermove', pointermove);
+        this.removeEventListener('pointerdown', pointerdown);
         if (lastDroppable) {
             let ordered = lastDroppable.classList.contains('destination-ordered');
             lastDroppable.appendChild(this);
@@ -98,10 +98,13 @@ function pointerdown(event) {
             if (!ordered) setChaosCoords(this, event);
         }
         else this.remove();
-        creator.appendChild(createDraggableNode());
-    };
+        portable = createDraggableNode();
+        creator.appendChild(portable);
+    }
 
+    this.addEventListener('pointerup', pointerup);
     document.addEventListener('pointermove', pointermove);
 }
 
-creator.appendChild(createDraggableNode());
+portable = createDraggableNode();
+creator.appendChild(portable);
