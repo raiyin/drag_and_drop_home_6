a522306074f66bda9268179da2881d896708cd17
Maxim P <raiyin@ya.ru>
2023-01-17 16:38:00 +0300

    Almost done. Prevent renew page error.

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ import { createRandomColor } from './utils/utils.js';
 
 let lastDraggableNodeId = 0;
 let creator = document.getElementById('creator');
-let target_ordered = document.getElementById('target_ordered');
-let target_chaos = document.getElementById('target_chaos');
+let portable = null;
+let lastDroppable = null;
 
 function genDraggableNodeId() {
     lastDraggableNodeId++;
@@ -11,77 +11,97 @@ function genDraggableNodeId() {
 }
 
 function createDraggableNode() {
-    let portable = document.createElement('div');
+    portable = document.createElement('div');
     portable.id = genDraggableNodeId();
     portable.classList += "portable";
     portable.setAttribute("draggable", "true");
     portable.style.backgroundColor = createRandomColor();
-    portable.addEventListener("dragstart", dragstart_handler);
+    portable.addEventListener('pointerdown', pointerdown);
+    portable.ondragstart = function () {
+        return false;
+    };
     return portable;
 }
 
-function computeChaosPosition(ev) {
-    let dragOffsetX = ev.dataTransfer.getData("offsetX");
-    let dragOffsetY = ev.dataTransfer.getData("offsetY");
-    let coordX = ev.layerX - dragOffsetX;
-    let coordY = ev.layerY - dragOffsetY;
-    if (ev.target.className === 'portable') {
-        // Если кидаем на дочерний.
-        coordX = coordX + ev.target.offsetLeft;
-        coordY = coordY + ev.target.offsetTop;
+function pointerdown(event) {
+    event.preventDefault();
+    let shiftX = event.clientX - portable.getBoundingClientRect().left;
+    let shiftY = event.clientY - portable.getBoundingClientRect().top;
+
+    setMovingStyle(portable);
+    document.body.append(portable);
+    moveAt(portable, event.pageX, event.pageY);
+    determineCurrentDroppable(event);
+
+    function setMovingStyle(element) {
+        element.style.boxShadow = '5px 5px 5px grey';
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
     }
-    return { coordX, coordY };
+
+    function removeMovingStyle(element, ordered) {
+        element.setAttribute("draggable", "false");
+        element.style.boxShadow = '';
+        element.style.cursor = 'default';
+        element.onpointerup = null;
+        element.style.position = ordered ? 'static' : 'absolute';
+    }
+
+    function moveAt(element, pageX, pageY) {
+        element.style.left = pageX - shiftX + 'px';
+        element.style.top = pageY - shiftY + 'px';
+    }
+
+    function enterDroppable(element) {
+        element.style.cursor = 'copy';
+    }
+
+    function leaveDroppable(element) {
+        element.style.cursor = 'move';
+    }
+
+    function setChaosCoords(element, event) {
+        // element.style.left = event.clientX - shiftX - event.target.offsetParent.offsetLeft + 'px';
+        // element.style.top = event.clientY - shiftY - event.target.offsetParent.offsetTop + 'px';
+
+        element.style.left = parseInt(element.style.left, 10) - event.target.offsetParent.offsetLeft + 'px';
+        element.style.top = parseInt(element.style.top, 10) - event.target.offsetParent.offsetTop + 'px';
+    }
+
+    function determineCurrentDroppable(ev) {
+        portable.hidden = true;
+        let elementBelow = document.elementFromPoint(ev.clientX, ev.clientY);
+        portable.hidden = false;
+
+        if (!elementBelow) return;
+        let droppableBelow = elementBelow.closest('.droppable');
+
+        if (droppableBelow != lastDroppable) {
+            if (lastDroppable) leaveDroppable(portable);
+            lastDroppable = droppableBelow;
+            if (lastDroppable) enterDroppable(portable);
+        }
+    }
+
+    function pointermove(event) {
+        event.preventDefault();
+        moveAt(portable, event.pageX, event.pageY);
+        determineCurrentDroppable(event);
+    }
+
+    portable.onpointerup = function (event) {
+        document.removeEventListener('pointermove', pointermove);
+        if (lastDroppable) {
+            let ordered = lastDroppable.classList.contains('destination-ordered');
+            lastDroppable.appendChild(this);
+            removeMovingStyle(this, ordered);
+            if (!ordered) setChaosCoords(this, event);
+        }
+        else this.remove();
+        creator.appendChild(createDraggableNode());
+    };
+
+    document.addEventListener('pointermove', pointermove);
 }
-
-function setChaosNodePosition(element, x, y) {
-    element.style.position = 'absolute';
-    element.style.top = y + 'px';
-    element.style.left = x + 'px';
-}
-
-function removeDragProps(element) {
-    element.removeEventListener("dragstart", dragstart_handler);
-    element.setAttribute("draggable", "false");
-    element.style.cursor = 'default';
-    element.style.boxShadow = '';
-}
-
-function dragstart_handler(ev) {
-    ev.dataTransfer.setData("text/html", ev.target.id);
-    ev.dataTransfer.setData("offsetX", ev.offsetX);
-    ev.dataTransfer.setData("offsetY", ev.offsetY);
-    this.style.boxShadow = '5px 5px 5px grey';
-}
-
-function dragover_handler(ev) {
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = "move";
-}
-
-function drop_handler_ordered(ev) {
-    ev.preventDefault();
-    const data = ev.dataTransfer.getData("text/html");
-    let portableNode = document.getElementById(data);
-    ev.currentTarget.appendChild(portableNode);
-    removeDragProps(portableNode);
-    creator.appendChild(createDraggableNode());
-}
-
-function drop_handler_chaos(ev) {
-    ev.preventDefault();
-    const data = ev.dataTransfer.getData("text/html");
-    let portableNode = document.getElementById(data);
-
-    let { coordX, coordY } = computeChaosPosition(ev);
-    setChaosNodePosition(portableNode, coordX, coordY);
-    ev.currentTarget.appendChild(portableNode);
-    removeDragProps(portableNode);
-    creator.appendChild(createDraggableNode());
-}
-
-target_ordered.addEventListener("drop", drop_handler_ordered, true);
-target_ordered.addEventListener("dragover", dragover_handler);
-target_chaos.addEventListener('drop', drop_handler_chaos);
-target_chaos.addEventListener('dragover', dragover_handler);
 
 creator.appendChild(createDraggableNode());
